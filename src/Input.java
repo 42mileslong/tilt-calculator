fe2cd7a46505e1af4f1abe4789e983ac2434527d
Rafi Long <rafilong42@gmail.com>
2015-01-11 16:04:02 -0800

    Wrote code to calculate idle force

---
+++ src/Input.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+/**
+ * Gets user input
+ */
+public class Input {
+
+    /**
+     * Returns the text entered by the user
+     * @return the text entered
+     */
+    public static String getNewString() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
+    /**
+     * Asks a question to the user and returns text answer
+     * @param message the question
+     * @return the answer
+     */
+    public static String question(String message) {
+        System.out.println(message);
+        return getNewString();
+    }
+
+    /**
+     * Asks a true false question that can be answered with "yes" and "no"
+     * @param message the question
+     * @return the true / false answer
+     */
+    public static String multipleChoiceQuestion(String message, String[] possibleAnswers) {
+        // a list of possible answers to give the user, compiled into a readable form
+        String listAnswers = "";
+        for (String possibleAnswer : possibleAnswers) {
+            listAnswers += possibleAnswer + "/";
+        }
+
+        // adds the possible answers in a readable form to the message, prints, and obtains the answer
+        message += " (" + listAnswers + ")";
+        String str = question(message);
+
+        // if the answer equals a possible answer, it returns the answer
+        for (String possibleAnswer : possibleAnswers) {
+            if (str.equals(possibleAnswer)) {
+                return possibleAnswer;
+            }
+        }
+
+        // if the answer does not equal a possible answer, it asks the user to print of the possible answers
+        System.out.println("Please enter " + listAnswers);
+        System.out.println();
+
+        //recursively calls itself, which it will do until it gets one of the possible answers
+        return multipleChoiceQuestion(message, possibleAnswers);
+    }
+
+    /**
+     * Asks a true false question
+     * @param message the message to be asked
+     * @return the true false answer
+     */
+    public static boolean trueFalse(String message) {
+        // calls the multiple choice question with possible answers being yes and no
+        String answer = multipleChoiceQuestion(message, new String[] {"yes", "no"});
+
+        // returns true if the answer is yes
+        return answer.equals("yes");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,39 @@
+/**
+ * Main class
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("These questions are about the part of the robot that extends on the outer side of the fulcrum");
+        double tipTorque = calcTorque(gravityForce(Double.parseDouble(Input.question("What is the weight of this part of the robot in kilograms?"))), Double.parseDouble(Input.question("How far is the center of mass of the robot past the fulcrum horizontally in meters?")), Double.parseDouble(Input.question("How far is the center of mass of the robot past the fulcrum vertically in meters?")));
+
+        System.out.println();
+        System.out.println("These questions are about the part of the robot on the other side of the fulcrum");
+        double stableTorque = calcTorque(gravityForce(Double.parseDouble(Input.question("What is the weight of this part of the robot in kilograms?"))), Double.parseDouble(Input.question("How far is the center of mass of the robot past the fulcrum horizontally in meters?")), Double.parseDouble(Input.question("How far is the center of mass of the robot past the fulcrum vertically in meters?")));
+
+        System.out.println();
+        boolean robotTip = findTip(tipTorque, stableTorque);
+
+        if (robotTip) {
+            System.out.println("The robot will tip while idle");
+            System.out.println("The torque on the tipping side is " + tipTorque + " Nm versus " + stableTorque + " Nm");
+        } else {
+            System.out.println("The robot will not not tip while idle");
+            System.out.println("The torque on the tipping side is " + tipTorque + " Nm versus " + stableTorque + " Nm");
+        }
+    }
+
+    public static boolean findTip(double tipTorque, double stableTorque) {
+        return tipTorque >= stableTorque;
+    }
+
+    public static double gravityForce(double weight) {
+        return weight * 9.8;
+    }
+
+    public static double calcTorque(double force, double xToPivot, double yToPivot) {
+        double angle = Math.sin(Math.atan2(xToPivot, yToPivot));
+        double diagonalDist = Math.sqrt(Math.pow(xToPivot, 2) + Math.pow(yToPivot, 2));
+        return force * diagonalDist * angle;
+    }
+}
